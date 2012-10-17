844ae8b1e925cfeb26512b839a4f3d612a67a7b6
Melvil <melvil662@gmail.com>
2012-10-17 14:43:30 +0900

    propel:generate-admin backend Category

---
+++ apps/backend/modules/category/lib/categoryGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * category module configuration.
+ *
+ * @package    bookmarks
+ * @subpackage category
+ * @author     Melvil
+ * @version    SVN: $Id: configuration.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class categoryGeneratorConfiguration extends BaseCategoryGeneratorConfiguration
+{
+}
